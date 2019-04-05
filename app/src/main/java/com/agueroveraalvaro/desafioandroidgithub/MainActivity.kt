e3ebfd94eefec1129e2c351e8b632d61dbfc2772
alvaro <aguero.vera.alvaro@gmail.com>
2019-04-05 14:10:19 -0300

    First commit, the project is empty.

---
+++ app/src/main/java/com/agueroveraalvaro/desafioandroidgithub/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.agueroveraalvaro.desafioandroidgithub
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.gson.Gson
+import java.util.ArrayList
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
